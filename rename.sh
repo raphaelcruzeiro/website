d57bacc456d724a2e4f2e540d025ec2baba74a1b
vagrant <vagrant@ubuntu-12.04.2-server-amd64>
2013-11-11 04:20:28 +0000

    Removed file

--- rename.sh
+++
@@ -1,3 +0,0 @@
-git filter-branch --commit-filter 'if [ "$GIT_AUTHOR_NAME" = "vagrant" ];
-  then export GIT_AUTHOR_NAME="Raphael Cruzeiro"; export GIT_AUTHOR_EMAIL=raphaelcruzeiro@raphaelcruzeiro.com;
-  fi; git commit-tree "$@"'
