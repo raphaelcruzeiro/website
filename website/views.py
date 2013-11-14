0d387d0a57d333054195e8886d013336ddfaca00
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2013-11-14 16:19:37 +0100

    Random cover

--- website/views.py
+++ website/views.py
@@ -9,6 +9,28 @@ import serializers
 class Index(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        import os
+        import random
+        from settings import STATIC_ROOT, STATICFILES_DIRS, DEBUG, STATIC_URL
+        static_dir = STATIC_ROOT
+
+        if DEBUG:
+            static_dir = STATICFILES_DIRS[0]
+
+        covers = ['%scss/covers/%s' % (STATIC_URL, c) for c in os.listdir(os.path.join(static_dir, 'css/covers')) if c.endswith('.css')]
+
+        context = super(Index, self).get_context_data(**kwargs)
+        if 'cover' in self.request.GET:
+            css_name = '%s.css' % self.request.GET['cover']
+            if css_name in covers:
+                context['cover'] = css_name
+                return context
+
+        random.shuffle(covers)
+        context['cover'] = covers[0]
+        return context
+
 
 class Contact(generics.CreateAPIView):
     serializer_class = serializers.ContactSerializer
